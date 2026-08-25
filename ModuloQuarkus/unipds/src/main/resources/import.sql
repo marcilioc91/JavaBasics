c2dc6a90493853987245511f379a679ad1d16bcb
Marcilio Cabral <marcilioccsilva@fac.pe.senac.br>
2026-08-24 23:29:53 -0300

    Ajustes propriedades

---
+++ ModuloQuarkus/unipds/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO pessoa (id, nome, ano_Nascimento) VALUES (1, 'MARCÍLIO', 1991);
+INSERT INTO pessoa (id, nome, ano_Nascimento) VALUES (2, 'MARIO', 1961);
+INSERT INTO pessoa (id, nome, ano_Nascimento) VALUES (3, 'MÁRCIO', 1988);
+INSERT INTO pessoa (id, nome, ano_Nascimento) VALUES (4, 'LAÍS', 1994);
+
+ALTER SEQUENCE pessoa_seq RESTART WITH 50;
